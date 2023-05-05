79c52efc45042c2fb5b0a67cd211832cb0b4b4b0
John Elmo Johnson <elmoj6@gmail.com>
2023-05-05 18:24:03 +1000

    Added routes and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  // <React.StrictMode>
-    <App />
-  // </React.StrictMode>,
-)
+// PAGES
+
+// Ungrouped
+import Home from "./pages/Home.tsx";
+import Search from "./pages/Search.tsx";
+import PageNotFound from "./pages/PageNotFound.tsx";
+
+// Account
+import Account from "./pages/account/Account.tsx";
+import Login from "./pages/account/Login.tsx";
+
+// Album
+import Albums from "./pages/album/Albums.tsx";
+import ViewAlbum from "./pages/album/ViewAlbum.tsx";
+
+// Artist
+import Artists from "./pages/artist/Artists.tsx";
+import ViewArtist from "./pages/artist/ViewArtist.tsx";
+
+// Playlist
+import Playlists from "./pages/playlist/Playlists.tsx";
+import ViewPlaylist from "./pages/playlist/ViewPlaylist.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "*",
+    element: <PageNotFound />
+  },
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/search",
+    element: <Search />
+  }
+])
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RouterProvider router={router} />
+);
