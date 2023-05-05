b0d66b89ff68fb07398b0b1d83ad437d588130aa
John Elmo Johnson <elmoj6@gmail.com>
2023-05-05 22:02:38 +1000

    Added user info and logout functionality

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,19 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { User } from "../../types";
+import spotify from "../lib/spotify";
+import {useNavigate} from "react-router-dom";
 
 const useUser = () => {
+  const navigate = useNavigate();
   const [user, setUser] = useState<User>();
   const [isLoading, setLoading] = useState<boolean>(false);
 
+  // Get user data
   const getUser = async () => {
     try {
-        
+      setLoading(true);
+      const result = await spotify.get("/me");
+      setUser(result.data);
     } catch (error) {
-        
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
+  };
+
+  // Logout
+  const logout = () => {
+    localStorage.clear();
+    navigate("/account/login")
   }
 
-  return {};
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  return { user, isLoading, logout };
 };
 
 export default useUser;
