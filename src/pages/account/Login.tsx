3a6e031c4fcd09f8a9775abff44254029c8d629e
John Elmo Johnson <elmoj6@gmail.com>
2023-05-05 21:25:58 +1000

    Updated Login page UI

--- src/pages/account/Login.tsx
+++ src/pages/account/Login.tsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import useLogin from '../../hooks/useLogin'
+import useLogin from "../../hooks/useLogin";
+import { SiSpotify } from "react-icons/si";
 
 const Login = () => {
-  const {url} = useLogin();
+  const { url } = useLogin();
 
   return (
-    <div>
-      {url && <a href={url}>Login</a>}
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col items-center space-y-4">
+        <div className="flex items-center space-x-2">
+          <SiSpotify className="font-bold text-3xl" />
+          <h1 className="text-2xl font-bold">Spotify Clone</h1>
+        </div>
+        {url && (
+          <a href={url} className="btn bg-primary text-white w-full">
+            Login
+          </a>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
