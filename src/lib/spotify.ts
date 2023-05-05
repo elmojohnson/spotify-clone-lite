a87615b861c4fb4af713334bf22a09e7b3b35ed3
John Elmo Johnson <elmoj6@gmail.com>
2023-05-05 22:21:43 +1000

    Added interceptor to refresh the expired token

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,10 +1,46 @@
 import axios from "axios";
 
+const accessToken = localStorage.getItem("access_token");
+const refreshToken = localStorage.getItem("access_token");
+
+// Instance
 const spotify = axios.create({
   baseURL: "https://api.spotify.com/v1/",
   headers: {
-    Authorization: "Bearer " + localStorage.getItem("access_token"),
+    Accept: "application/json",
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + accessToken,
   },
 });
+
+// Interceptor
+spotify.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const { response, config } = error;
+
+    // Refresh the access token if expired
+    if (response.status === 401) {
+      if (refreshToken) {
+        try {
+          const result = await axios.post("/api/auth/refresh_token", {
+            refresh_token: refreshToken,
+          });
+
+          if (result.data.access_token) {
+            localStorage.setItem("access_token", result.data.access_token);
+            config.headers["Authorization"] = `Bearer ${result.data.access_token}`;
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      }
+
+      return spotify(config);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default spotify;
