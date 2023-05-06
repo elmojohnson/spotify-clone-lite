bbfd1d5999289716874dae88ad7b59bfb491d3b3
John Elmo Johnson <elmoj6@gmail.com>
2023-05-06 18:02:34 +1000

    Dynamic title for lists

--- src/components/artist/ArtistList.tsx
+++ src/components/artist/ArtistList.tsx
@@ -2,13 +2,14 @@ import { Artist } from "../../../types";
 import ArtistItem from "./ArtistItem";
 
 type Props = {
+  title: string;
   artists: Artist[];
 };
 
-const ArtistList = ({ artists }: Props) => {
+const ArtistList = ({ artists, title }: Props) => {
   return (
     <div className="w-full">
-      <h2 className="font-bold text-lg mb-4">My Top 10 Artists</h2>
+      <h2 className="font-bold text-lg mb-4">{title}</h2>
       <div className="flex flex-col space-y-3">
         {artists.map((artist, i) => {
           return <ArtistItem key={i} {...artist} />;
