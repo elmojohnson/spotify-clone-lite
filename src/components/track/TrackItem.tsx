9759d5f032ad0b5f726ec0a74fc79127976aafe9
John Elmo Johnson <elmoj6@gmail.com>
2023-05-06 18:13:40 +1000

    Added play and menu button

--- src/components/track/TrackItem.tsx
+++ src/components/track/TrackItem.tsx
@@ -1,16 +1,35 @@
-import {FC} from 'react'
-import { Track } from '../../../types'
+import { FC } from "react";
+import { Track } from "../../../types";
 
-const TrackItem:FC<Track> = (track) => {
+import { motion } from "framer-motion";
+import { MdPlayCircle, MdMoreVert } from "react-icons/md";
+
+const TrackItem: FC<Track> = (track) => {
   return (
     <div className="flex items-center border hover:shadow">
       <img src={track.album.images[1].url} className="w-14" />
-      <div className="px-4">
-        <p className="font-semibold">{track.name}</p>
-        <span className="text-sm">{track.artists.map((artist) => artist.name).join(", ")}</span>
+      <div className="flex items-center justify-between w-full">
+        <div className="w-full leading-none px-2">
+          <p className="font-semibold">{track.name}</p>
+          <span className="text-sm">
+            {track.artists.map((artist) => artist.name).join(", ")}
+          </span>
+        </div>
+        <div className="px-2 flex items-center">
+          <motion.button
+            whileHover={{ scale: 1.2 }}
+            whileTap={{ scale: 1 }}
+            className="text-4xl text-primary hover:brightness-90"
+          >
+            <MdPlayCircle />
+          </motion.button>
+          <button className="text-xl hover:brightness-90">
+            <MdMoreVert />
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TrackItem
+export default TrackItem;
