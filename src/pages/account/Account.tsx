329b16fb25715b71a4dd4fbf49b273b2bb71c573
John Elmo Johnson <elmoj6@gmail.com>
2023-05-06 11:11:20 +1000

    Updated account page UI

--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -4,19 +4,29 @@ import useUser from "../../hooks/useUser";
 const Account = () => {
   const { user, isLoading, logout } = useUser();
 
-  if(isLoading) {
-    return <LoadingState />
+  if (isLoading) {
+    return <LoadingState />;
   }
 
   return (
     <div>
       <h1 className="title">Account</h1>
-      <div className="flex flex-col items-center">
-        <img src={user?.images[0].url} className="w-24 rounded-full" />
-        <p className="text-xl font-semibold">{user?.display_name}</p>
-        <p className="text-sm">{user?.email}</p>
-        <button className="btn mt-4" onClick={logout}>Logout</button>
+      <div className="flex items-start justify-between p-4 bg-white border rounded">
+        <div className="flex items-center space-x-4">
+          <img src={user?.images[0].url} className="w-24 rounded-full" />
+          <div className="leading-none">
+            <p className="md:text-xl text-2xl font-bold">{user?.display_name}</p>
+            <p className="md:text-xs text-md">{user?.email}</p>
+          </div>
+        </div>
+        <button className="md:block hidden btn px-2 py-1 border-2 border-primary text-primary bg-transparent hover:bg-primary hover:text-white hover:brightness-100">
+          Logout
+        </button>
       </div>
+
+      <button className="mt-4 w-full md:hidden block btn px-2 py-1 border-2 border-primary text-primary bg-transparent hover:bg-primary hover:text-white hover:brightness-100">
+          Logout
+        </button>
     </div>
   );
 };
