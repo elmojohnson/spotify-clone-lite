973a194e4d53206153cf4d0cc3e74358b67c940d
John Elmo Johnson <elmoj6@gmail.com>
2023-05-06 19:17:47 +1000

    Browse categories

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import useUserTopItems from "../hooks/useUserTopItems";
 import LoadingState from "../components/utils/LoadingState";
 import TrackList from "../components/track/TrackList";
 import ArtistList from "../components/artist/ArtistList";
+import CategoryList from "../components/category/CategoryList";
 
 const Home = () => {
   const { tracks, artists, isLoading } = useUserTopItems();
@@ -12,11 +13,12 @@ const Home = () => {
   }
 
   return (
-    <div>
+    <div className="flex flex-col space-y-10">
       <div className="flex lg:flex-row flex-col lg:space-x-4 space-x-0 lg:space-y-0 space-y-10">
         <TrackList title="My Top Tracks" tracks={tracks} />
         <ArtistList title="My Top Artists" artists={artists} />
       </div>
+      <CategoryList />
     </div>
   );
 };
