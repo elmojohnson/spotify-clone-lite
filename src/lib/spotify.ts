f8c10394d0f935875692eb6308eebd13b0b926ac
John Elmo Johnson <elmoj6@gmail.com>
2023-05-06 10:36:45 +1000

    Added interceptor for refreshing token

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
+import { Buffer } from "buffer";
 
+const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
+const client_secret = import.meta.env.VITE_SPOTIFY_CLIENT_SECRET;
 const accessToken = localStorage.getItem("access_token");
-const refreshToken = localStorage.getItem("access_token");
+const refreshToken = localStorage.getItem("refresh_token");
 
 // Instance
 const spotify = axios.create({
@@ -13,7 +16,13 @@ const spotify = axios.create({
   },
 });
 
-// Interceptor
+spotify.interceptors.request.use((config) => {
+  let token = localStorage.getItem("access_token");
+  config.headers["Authorization"] = "Bearer " + token;
+  return config;
+});
+
+// Interceptor to refresh token
 spotify.interceptors.response.use(
   (response) => response,
   async (error) => {
@@ -23,13 +32,29 @@ spotify.interceptors.response.use(
     if (response.status === 401) {
       if (refreshToken) {
         try {
-          const result = await axios.post("/api/auth/refresh_token", {
-            refresh_token: refreshToken,
-          });
+          const result = await axios.post(
+            "https://accounts.spotify.com/api/token",
+            {
+              grant_type: "refresh_token",
+              refresh_token: refreshToken,
+            },
+            {
+              headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+                Authorization:
+                  "Basic " +
+                  new Buffer(client_id + ":" + client_secret).toString(
+                    "base64"
+                  ),
+              },
+            }
+          );
 
           if (result.data.access_token) {
             localStorage.setItem("access_token", result.data.access_token);
-            config.headers["Authorization"] = `Bearer ${result.data.access_token}`;
+            config.headers[
+              "Authorization"
+            ] = `Bearer ${result.data.access_token}`;
           }
         } catch (error) {
           console.error(error);
